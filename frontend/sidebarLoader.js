03594e55938d90fefd22d62efdbc4adba87ac5b5
ddelux <cheinzmann3@gmail.com>
2013-07-13 12:14:48 -0700

    last shot at fixing weird bug

--- frontend/sidebarLoader.js
+++ frontend/sidebarLoader.js
@@ -1,50 +1,32 @@
 $(document).ready( function() {
-  
-  var xml;
-  var xls;
-  $.ajax({
-   url: "/widgets/widget_names.xml",
-   success: function(data) {
-     xml = data;
-     $.ajax({
-       url: "/widgets/widget_names.xslt",
-       success: function(data) {
-         xls = data;
-         xsltProcessor=new XSLTProcessor();
-         xsltProcessor.importStylesheet(xls);
-         var resultDocument = xsltProcessor.transformToFragment(xml,document);
-         //alert(resultDocument);
-         $('.sidebarContent').append(resultDocument);
-         //document.getElementById("sidebarContent").appendChild(resultDocument);
-       }
-     });
-    }
-  });
-
-
-$.get('/widgets/widget_names.xml')
- .done(function(data){    
-  // this function is executed if the request was sucessfull
-  //alert("yay!");
- })
- .fail(function(){
- //alert("Oh no!");
- // this function is executed if the request fails
- });
-            
-
-
-$.get('/widgets/widget_names.xml')
-.done(function(data){
-  // parse the xml
-  data = $.parseXML(data);
- // do anything you want with the parsed data
- //alert(data);
- $('.sidebarContent').append(data);
-  })
-  .fail(function(){
-  //alert('something went wrong!');
-   })    ;
-  
- 
+	var xmlData;
+	$.ajax({
+		url: "/widgets/widget_names.xml",
+		dataType: "xml",
+		async: false,
+		success: function(data) {
+			xmlData = data;
+		}
+	});
+	
+	var xsltData;
+	$.ajax({
+		url: "/widgets/widget_names.xslt",
+		dataType: "xml",
+		async: false,
+		success: function(data) {
+			xsltData = data;
+		}
+	});
+	
+	alert(xmlData);
+	alert(xsltData);
+	
+	xsltProcessor = new XSLTProcessor();
+	xsltProcessor.importStylesheet(xsltData);
+	var resultDocument = xsltProcessor.transformToFragment(xmlData, document);
+	
+	alert(resultDocument);
+	
+	$(".sidebarContent").append(resultDocument);
 });
