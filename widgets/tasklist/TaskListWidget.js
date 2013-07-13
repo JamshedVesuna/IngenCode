4af939ce624c5e2e9546f08ef48b683f6db9d926
ddelux <cheinzmann3@gmail.com>
2013-07-13 09:28:14 -0700

    TaskListWidget improvements
    made TaskListWidget load internal content without async and added
    widget.itemTemplate for quick and easy duplication. (for creating
    multiple tasks)

--- widgets/tasklist/TaskListWidget.js
+++ widgets/tasklist/TaskListWidget.js
@@ -3,12 +3,25 @@
 function TaskListWidget(title) {
 	var widget = Object.create(new Widget(title));
 	
-	// put the task list inside the thingy
-	$(".widgetContent", widget.html).load("/widgets/tasklist/content.html");
+	// load the content with async off and add into widget content div
+	var rawHTML;
+	$.ajax({
+		url: "/widgets/tasklist/content.html",
+		async: false,
+		success: function(data) {
+			rawHTML = data;
+		}
+	});
+	$(".widgetContent", widget.html).append(rawHTML);
+	
+	// init the 
 	widget.list = new Array();
 	
 	// plug the widget in
-	widget.div.object = widget;
+	widget.div.object = widget;	
+	
+	widget.itemTemplate = $("#itemTemplate", widget.html)[0];
+	$("<table></table>").append(widget.itemTemplate);
 	
 	return widget;
 }
