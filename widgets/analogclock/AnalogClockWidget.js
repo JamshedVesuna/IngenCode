04bfd1fde6a6108aa44c2f018f7579168d625823
ddelux <cheinzmann3@gmail.com>
2013-07-15 08:08:45 -0700

    Added content for AnalogClockWidget
    images, content container, and corresponding JS class

--- widgets/analogclock/AnalogClockWidget.js
+++ widgets/analogclock/AnalogClockWidget.js
@@ -1,4 +1,16 @@
 // JavaScript Document
 
 function AnalogClockWidget() {
+	var widget = Object.create(new Widget());
+	
+	var rawHTML;
+	$.ajax({
+		url: "/widgets/analogclock/content.html",
+		async: false,
+		success: function(data) {
+			rawHTML = data;
+		}
+	});
+	
+	$(".widgetContent", widget.html).append(rawHTML);
 }
