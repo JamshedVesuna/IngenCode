f8fafafa25563062f1c833fa1129e416d2d7b3dc
JamshedVesuna <jamshed.vesuna@gmail.com>
2013-07-16 01:44:24 -0700

    This should work

--- frontend/ui.js
+++ frontend/ui.js
@@ -1,9 +1,19 @@
+function bindS(){
+  $(document).bind('keydown', 's', function (){
+    alert("worked");
+    $('.sidebar').animate({width: 'toggle'});
+
+  });
+}
+
 $(document).bind('keydown','s', function (){
+
   $('.sidebar').animate({width: 'toggle'});
   });
 
 
 $(document).ready(function() {
+  $('#bindS').bind('click', bindS);
   $('#expand').click(function () {
   $('.sidebar').animate({ width: 'toggle'});
  });
