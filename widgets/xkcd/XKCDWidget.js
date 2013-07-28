ee53b9364ddf4f5200e5716db7880b7706f538ad
ddelux <cheinzmann3@gmail.com>
2013-07-28 20:15:45 +0000

    XKCD article widget now resizes to fit comic size
    used an onload function for the image

--- widgets/xkcd/XKCDWidget.js
+++ widgets/xkcd/XKCDWidget.js
@@ -2,25 +2,31 @@
 function XKCDWidget() {
 	var widget = Object.create(new Widget("XKCD"));
 	
+	// get the JSON information from xkcd.com
 	var comicObject;
 	$.ajax({
 		url: "http://dynamic.xkcd.com/api-0/jsonp/comic",
 		async: false,
 		dataType: "jsonp",
 		success: function(data) {
-			comicObject = data;
-			console.log(comicObject);
+			comicObject = data;			
 			
-				
+			// create the image object	
 			var image = $("<img>");
-			//$(image).load(widget.sizeDivAppropriately);
-			$(image).load(function() {
-				$(widget.div).height($(".widgetContent", widget.html).height() + $(".widgetTitle", widget.html).height());
+			// once the image has finished loading, resize the div to fit the content
+			$(image).load( function() {
+				var widgetTitle = $(".widgetTitle", widget.html);
+				var widgetContent = $(".widgetContent", widget.html);
+				
+				$(widget.div).height(widgetTitle.height() + widgetContent.height());
+				$(widget.div).width($("img",widget.html).width());
 			});
 			image.attr("src", comicObject.img);
 			
+			// append inside the content container
 			$(".widgetContent", widget.html).append(image);
 			
+			// and change the title
 			widget.setTitle("XKCD - " + comicObject.safe_title);
 		}
 	});
